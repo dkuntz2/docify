72277b2a5c3836b7d003f5b0dc05a5a90669649e
Don Kuntz <don@dkuntz2.com>
2011-11-01 21:21:52 -0500

    java finalized for version1

---
+++ java/javadocify.py
@@ -0,0 +1,163 @@
+# javadocify - as implemented in python?
+
+from sys import argv
+import re
+
+#files = []
+inArr = argv
+
+
+files = []
+
+for f in inArr:
+	s = f.split(".")
+	if len(s) > 1:
+		if s[len(s) - 1] == "java" :
+			files.append(f);
+
+
+for f in files:
+	file = open(f);
+	
+	text = "";
+	for line in file :
+		text = text + line
+
+
+	t = text.split("/**\n");
+	t.pop(0)
+
+	blocks = []
+	for e in t:
+		tmp = e.split("*/");
+		tmpStuffs = tmp[1].split("{")
+		blocks.append(tmp[0] + "\n\n" + tmpStuffs[0])
+	
+	pathList = f.split("/")
+	fname = pathList[len(pathList) - 1]
+
+
+	writer = open(fname.replace(".java", ".md"), "w")
+	for b in blocks:
+		numTabs = 0;
+
+		tmpInt = b.index("\n")
+
+		tmp = b[0:int(tmpInt)]
+
+		moreTabs = True
+
+		while moreTabs:
+			try :
+				if tmp.index("\t") == 0:
+					numTabs += 1
+					tmp = tmp[1:len(tmp)]
+			except ValueError:
+				moreTabs = False
+		
+		# Get fun method stuff
+		methodHeader = b.split("\n\n")[len(b.split("\n\n")) - 1]
+		methodHeader = methodHeader.replace("\n" + ("\t" * (numTabs - 1)), "")
+
+		#public or private
+		pubpriv = ("public" if methodHeader.index("public") == 0 else "private")
+		
+		# is it static
+		static = False
+		try :
+			static = True if methodHeader.index("static") > -1 else False
+		except ValueError :
+			static = False
+
+		# return type
+		numCharsBefore = len(pubpriv) + 1
+		if static :
+			numCharsBefore += 7
+		retType = methodHeader[numCharsBefore:len(methodHeader)].split(" ")[0]
+		
+		# methodName
+		numCharsBefore += len(retType) + 1
+		methodName = methodHeader[numCharsBefore:len(methodHeader)].split("(")[0]
+
+		# param type
+		paramtype = {}
+		paramreg1 = re.search('([a-zA-Z0-9,\ ]+)', methodHeader)
+		paramreg2 = re.search('([a-zA-Z0-9,\ ]+)', methodHeader.replace(paramreg1.group(), ""))
+		params = paramreg2.group().replace(", ", ",").split(",")
+		for p in params:
+			tmpp = p.split(" ")
+			paramtype[tmpp[1]] = tmpp[0]
+			
+		e = "\n" + b
+
+		# grab the $parameters block area
+		param = {}
+		para = re.search('@parameters\s=\s\[[^\]]*', e)
+		if para != None :
+			paramStr = para.group();
+			paramStart = re.search('@parameters\s=\s\[', paramStr)
+			paramStr = paramStr[paramStart.span()[1]:len(paramStr)].replace("\n" + ("\t" * (numTabs + 1)), "\n")
+
+			# place individual parameters in dict param
+			tmpPara = paramStr.split("\n\n")
+
+			for t in tmpPara :
+				varName = re.search('[^:]*', t)
+				varVal = t[varName.span()[1]:len(t)]
+				varName = varName.group().replace(" ", "").replace("\n", "")
+				varVal = re.search('\s[^:]*', varVal).group()
+				param[varName + " - " + paramtype[varName]] = varVal
+		# grab the @return block area
+		retur = {}
+		ret = re.search('@returns\s=\s\[[^\]]*', e)
+		if ret != None :
+			retStr = ret.group()
+			retStart = re.search('@returns\s=\s\[', retStr)
+			retStr = retStr[retStart.span()[1]:len(retStr)].replace("\n" + ("\t" * (numTabs + 1)), "\n")
+
+			tmpRet = retStr.split("\n\n")
+
+			for r in tmpRet:
+				caseName = re.search('[^:]*', r)
+				caseVal = r[caseName.span()[1]:len(r)]
+				caseVal = re.search('\s[^:]*', caseVal).group()
+				retur[caseName.group().replace("\n", "")] = caseVal
+
+		
+		if ret != None :
+			# remove returns block from b
+			e = e[0:ret.span()[0]]
+
+		if para != None:
+			# set b to everything before the parameters
+			e = e[0:para.span()[0]]
+
+
+		# write method name and other fun stuff
+		writer.write("# " + pubpriv + (" static " if static else " ") + methodName + " ( ")
+		i = 0
+		for k in paramtype:
+			i += 1
+			writer.write(paramtype[k] + " " + k)
+			if (i < len(paramtype)):
+				writer.write(", ")
+
+		writer.write(" )\n")
+		# write out the docify block
+		writer.write(e.replace("\n" + ("\t" * numTabs), "\n"))
+
+		# parameters
+		writer.write("## Parameters\n")
+		for k in param:
+			writer.write("\n### " + k + "\n\n" + param[k][1:len(param[k])] + "\n")
+
+		# returns
+		writer.write("\n## Returns - " + retType + "\n")
+		for k in retur:
+			writer.write("### " + k + "\n\n" + retur[k][1:len(retur[k])] + "\n")
+		
+		#writer.write(b.split("\n\n")[len(b.split("\n\n")) - 1])
+		# separater
+		if blocks.index(b) < len(blocks) - 1:
+			writer.write("-----\n\n")
+	
---
+++ java/test/javasample.java
@@ -0,0 +1,55 @@
+public class javasample {
+	
+	public static void main(String[] args) {
+		System.out.println(factorial(10));
+	}
+
+	/**
+		Get a factorial *recursively*
+
+		Standard factorial method using recursion, returns 1 if x is less than
+		or equal to 1, otherwise it returns `factorial(x-1)`.
+
+		@parameters = [
+			x : integer to the the factorial of.
+		]
+
+		@returns = [
+			default : x!
+		]
+	*/
+	public static int factorial(int x) {
+		if (x <= 1)
+			return 1;
+		else
+			return factorial(x-1);
+	}
+
+	/**
+		Jumble up a string
+
+		Currently it doesn't do anything, as the whole method is
+
+			public static String jumble(String toJumble, int numTimes) {
+				return toJumble;
+			}
+
+		However in the possible future, it might do something, something
+		really cool like. YEAH.
+
+		@parameters	= [
+			toJumble: The word to jumble up
+
+			numTimes: The number of times to jumble the word
+		]
+
+		@returns	= [
+			default 	: the toJumble String passed in the method header
+			
+			Case0 	: Nothing, there is no case other than the standard one
+		]
+	*/
+	public String jumble(String toJumble, int numTimes) {
+		return toJumble;
+	}
+}
